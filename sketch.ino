4f9adfd8b63cd95090b0da8b9d2dbe2c6d946d5e
Petrovich <132599168+petrowhich@users.noreply.github.com>
2023-10-04 17:02:23 +0300

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,46 @@
+/*
+   Macros
+*/
+#define arrayLength(a) (sizeof(a) / sizeof(a[0]))
+
+/*
+   Constants
+*/
+#define SPK_PIN 12
+#define POWER_LED_PIN 13
+#define BPM 100
+
+/*
+   Pattern, timing and playback
+*/
+const int drumPattern[] = {
+  1, 0, 1, 0, 1, 1, 0, 0
+};
+const size_t drumPatternLength = arrayLength(drumPattern);
+const int estimatedTime = 35;
+const float noteDelayMs = 60000.0 / float(BPM * 2) - estimatedTime;
+#define drumHit tone(SPK_PIN, 2000, estimatedTime)
+#define skipHit delay(estimatedTime)
+
+/*
+   Arduino functions
+*/
+void setup()
+{
+  pinMode(SPK_PIN, OUTPUT);
+  pinMode(POWER_LED_PIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(POWER_LED_PIN, HIGH);
+  for (size_t i = 0; i < drumPatternLength; i++)
+  {
+    if (drumPattern[i])
+      drumHit;
+    else
+      skipHit;
+
+    delay(noteDelayMs);
+  }
+}
